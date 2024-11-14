e5857cc1a91cbe8df80a2b95c9887d71ca516f68
Keittiokyynikko <karvinen.juuso@gmail.com>
2024-11-14 12:20:52 +0200

    eslint

--- osa3/models/person.js
+++ osa3/models/person.js
@@ -6,41 +6,37 @@ const url = process.env.MONGODB_URI
 console.log(`connecting to ${url}`)
 
 mongoose.connect(url)
-.then(result => {
+  .then(result => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-function validator1(val) {
-    return /\d{2,3}-\d{7,8}/.test(val)
-}
-
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function validator1(val) {
+        return /\d{2}-\d{7}/g.test(val)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        minlength: 8,
-        validate: {
-            validator: function validator1(val) {
-                return /\d{2}-\d{7}/g.test(val)
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-    },
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-      }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
--- osa3/mongo.js
+++ osa3/mongo.js
@@ -9,31 +9,31 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
-  
+
 const Person = mongoose.model('Person', noteSchema)
 
 if(process.argv.length<4) {
-    console.log("phonebook:")
+  console.log('phonebook:')
 
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 
 } else {
-    
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4],
-    })
-    
-    person.save().then(result => {
-        console.log(`added ${person.name} ${person.number} to phonebook`)
-        mongoose.connection.close()
-    })
+
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+
+  person.save().then(result => {
+    console.log(`added ${person.name} ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
